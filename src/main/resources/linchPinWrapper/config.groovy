d7f2e2782b93e44918e55ea226a923f60817348a
Aviel Yosef <avielyosef@dhcp-4-38.tlv.redhat.com>
2018-10-08 11:14:29 +0300

    Change user interface && adding options for PinFile

--- src/main/resources/linchPinWrapper/config.groovy
+++ src/main/resources/linchPinWrapper/config.groovy
@@ -3,7 +3,7 @@ f=namespace(lib.FormTagLib)
 
 def installations = app.getDescriptorByType(Installation.linchPinTool.DescriptorImpl).installations
 
-if (installations.length > 1) {
+if (installations.length != 0) {
     f.entry(title: _('Which LinchPin:')) {
         select(class: 'setting-input', name: 'Installation.linchPinTool.installation') {
             installations.each { install ->
@@ -13,24 +13,6 @@ if (installations.length > 1) {
             }
         }
     }
-    f.entry(title:_("PinFile:"),field:"pinfile") {
-        f.textarea()
-    }
-    f.advanced{
-        f.entry(title:_("Topology File Name:"),field:"topologyFileName"){
-            f.textbox()
-        }
-        f.entry(title:_("Topology File:"),field:"topologyFile") {
-            f.textarea()
-        }
-        f.entry(title:_("Layout File Name:"),field:"layoutFileName"){
-            f.textbox()
-        }
-        f.entry(title:_("Layout File:"),field:"layoutFile") {
-            f.textarea()
-        }
-    }
-}else if (installations.length != 0){
     f.entry(title:_("PinFile:"),field:"pinfile") {
         f.textarea()
     }
